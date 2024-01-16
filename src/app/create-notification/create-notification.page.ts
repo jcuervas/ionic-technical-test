11cb20a55cb0661edf149c10fbde0200f21b0a79
Manuel Humanes Cabrera <manuel.humanescabrera@gmail.com>
2024-01-16 19:04:10 +0100

    Add create notification feature and form validation

--- src/app/create-notification/create-notification.page.ts
+++ src/app/create-notification/create-notification.page.ts
@@ -1,6 +1,9 @@
 import {Component} from '@angular/core';
 import {IonicModule} from '@ionic/angular';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { NotificationsService } from '../services/notifications.service';
+import { Router } from '@angular/router';
+import { delay } from 'rxjs';
 
 @Component({
   selector: 'app-create-notification',
@@ -10,11 +13,37 @@ import {ReactiveFormsModule} from '@angular/forms';
   imports: [IonicModule, ReactiveFormsModule]
 })
 export class CreateNotificationPage {
-
-  constructor() {}
+  form: FormGroup;
+  isLoading = false;
+  constructor(private readonly fb: FormBuilder, private readonly notificationsService: NotificationsService, private readonly router: Router) {
+    this.form = this.fb.group({
+      title: ['', [Validators.required]],
+      body: ['', [Validators.required]]
+    })
+  }
 
   createNotification() {
-
+    this.isLoading = true;
+    const { title, body } = this.form.value;
+    const notification: LocalNotification = {
+      title,
+      body,
+      date: new Date(),
+      status: 'unread'
+    }
+    this.notificationsService.addNotification(notification).pipe(
+      delay(2000)
+    ).subscribe({
+      next: () => {
+        this.router.navigate(['/notifications-list'])
+      },
+      error: (err) => {
+        console.error('Error creating notification: ', { err })
+      },
+      complete: () => {
+        this.isLoading = false;
+      }
+    })
   }
 
 }
